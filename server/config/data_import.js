707eb6d4cb026d62defcd61364ac42a875cebf5e
Renan Bara <renan.bara@hotmail.com>
2016-12-19 11:54:39 -0200

    ...

--- server/config/data_import.js
+++ server/config/data_import.js
@@ -247,7 +247,7 @@ export default function(app) {
 
   }
 
-  var textSched = later.parse.text('every 30 sec');
+  var textSched = later.parse.text('every 1 min');
   var timer = later.setInterval(task, textSched);
 }
 
